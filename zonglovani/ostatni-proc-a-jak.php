4f3ee5a44a46b0ea71ae25c2ff5a0fc8c0cc4977
Petr <petr@kle.cz>
2009-02-20 20:22:35 +0000

    Doplnění starších změn na stránku historie.

--- zonglovani/ostatni-proc-a-jak.php
+++ zonglovani/ostatni-proc-a-jak.php
@@ -22,7 +22,9 @@ titulek(__FILE__);
 Kdy¾ jsem zaèínal ¾onglovat (zima 2004), hodnì mi pomohli informace z internetu. Bylo snadné najít <a href="/zonglovani/odkazy.html" title="Dal¹í stránky o ¾onglování.">dobré stránky o ¾onglování</a>, ale ¾ádná nebyla èesky!
 </p>
 
-<p>Proto zapisuji triky s míèky, kruhy a ku¾ely. Navíc nakreslení obrázkù a popsání triku mi pomáhá pøi uèení. Tak postupnì vznikl ¾onglérùv slabikáø.</p>
+<p>Proto jsem zaèal zapisovat triky s míèky, kruhy a ku¾ely. Navíc nakreslení obrázkù a popsání triku mi pomáhá pøi uèení. Tak postupnì vznikl ¾onglérùv slabikáø.</p>
+
+<p>Od bøezna 2007 zaznamenávám i <a href="/zonglovani/changelog.html" title="Seznam zmìn v ¾onglérovì slabikáøi.">historii zmìn</a>.</p>
 
 </div>
 
