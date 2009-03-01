4a6847b855d3e4c4e7bfdf29458669cbb51b594e
Petr <petr@kle.cz>
2009-03-01 14:52:47 +0000

    Náhledy odkazovaných stránek

--- zonglovani/ostatni-odkazy.php
+++ zonglovani/ostatni-odkazy.php
@@ -24,78 +24,71 @@ titulek(__FILE__);
 
 <!-- slabikar-online-link -->
 
-<dt>
-<a href="http://zongluj.eu" title="Abryho stránky o ¾onglování." class="external" onclick="pageTracker._trackPageview('/goto/zongluj.eu');">zongluj.eu</a>
-</dt>
-<dd>
+<h5><a href="http://zongluj.eu" title="Abryho stránky o ¾onglování." class="external" onclick="pageTracker._trackPageview('/goto/zongluj.eu');">zongluj.eu</a></h5>
+<p>
+<a href="http://zongluj.eu" title="Abryho stránky o ¾onglování." onclick="pageTracker._trackPageview('/goto/zongluj.eu');"><? echo img("odkazy-zongluj.eu.png","zongluj.eu"); ?></a>
 Abryho stránky o ¾onglování. Animace ¾onglování s míèky. Doplnìné krátkými popisky.
-</dd>
+</p>
 
-<dt>
+<h5>
 <a href="http://www.zongluj.wz.cz/juggle.php?page=novinky" title="zongluj.wz.cz" class="external" onclick="pageTracker._trackPageview('/goto/zongluj.wz.cz');">zongluj.wz.cz</a>
-</dt>
-<dd>
+</h5>
+<p>
+<a href="http://www.zongluj.wz.cz/juggle.php?page=novinky" title="zongluj.wz.cz" onclick="pageTracker._trackPageview('/goto/zongluj.wz.cz');"><? echo img("odkazy-zongluj.wz.cz.png","zongluj.wz.cz"); ?></a>
 Davidovi stránky o ¾onglování.
-</dd>
+</p>
 
-<dt>
+<h5>
 <a href="http://www.zonglovanie.sk" title="Slovenský server." class="external" onclick="pageTracker._trackPageview('/goto/zonglovanie.sk');">zonglovanie.sk</a>
-</dt>
-<dd>
+</h5>
+<p>
+<a href="http://www.zonglovanie.sk" title="Slovenský server." onclick="pageTracker._trackPageview('/goto/zonglovanie.sk');"><? echo img("odkazy-zonglovanie.sk.png","zonglovanie.sk"); ?></a>
 Skvìlá <a href="literatura.html" title="Dal¹í literatura o ¾onglování.">uèebnice ¾onglování</a> pro samouky. Dostupná ve formátu pdf.
-</dd>
+</p>
 
-<dt>
-<a href="http://didier.arlabosse.free.fr/balles/english/" title="Anglické stránky o ¾onglování." class="external" onclick="pageTracker._trackPageview('/goto/didier.arlabosse.free.fr');">didier.arlabosse.free.fr/balles/english</a>
-</dt>
-<dd>
+<h5><a href="http://didier.arlabosse.free.fr/balles/english/" title="Anglické stránky o ¾onglování." class="external" onclick="pageTracker._trackPageview('/goto/didier.arlabosse.free.fr');">didier.arlabosse.free.fr/balles/english</a></h5>
+<p>
+<a href="http://didier.arlabosse.free.fr/balles/english/" title="Anglické stránky o ¾onglování." onclick="pageTracker._trackPageview('/goto/didier.arlabosse.free.fr');"><? echo img("odkazy-didier.png","didier.arlabosse.free.fr/balles/english"); ?></a>
 Didierovi stránky o ¾onglování - anglicky a francouzsky.
-</dd>
+</p>
 
-<dt>
-<a href="http://www.jugglingdb.com" title="Anglické stránky o ¾onglování." class="external" onclick="pageTracker._trackPageview('/goto/jugglingdb.com');">Internet Juggling Database</a>
-</dt>
-<dd>
+<h5><a href="http://www.jugglingdb.com" title="Anglické stránky o ¾onglování." class="external" onclick="pageTracker._trackPageview('/goto/jugglingdb.com');">Internet Juggling Database</a></h5>
+<p>
+<a href="http://www.jugglingdb.com" title="Anglické stránky o ¾onglování." onclick="pageTracker._trackPageview('/goto/jugglingdb.com');"><? echo img("odkazy-jugglingdb.com.png","jugglingdb.com"); ?></a>
 Bezedná studna informací o ¾onglování.
-</dd>
+</p>
 
-<dt>
-<a href="http://www.juggling.org/" title="Anglické stránky o ¾onglování." class="external" onclick="pageTracker._trackPageview('/goto/juggling.org');">Juggling Information Service</a>
-</dt>
-<dd>
+<h5><a href="http://www.juggling.org/" title="Anglické stránky o ¾onglování." class="external" onclick="pageTracker._trackPageview('/goto/juggling.org');">Juggling Information Service</a></h5>
+<p>
+<a href="http://www.juggling.org/" title="Anglické stránky o ¾onglování." onclick="pageTracker._trackPageview('/goto/juggling.org');"><? echo img("odkazy-juggling.org.png","juggling.org"); ?></a>
 Informace pro ¾ongléry.
-</dd>
+</p>
 
-<dt>
-<a href="http://www.passingdb.com/" title="Anglické stránky o ¾onglování." class="external" onclick="pageTracker._trackPageview('/goto/passingdb.com');">passingdb.com</a>
-</dt>
-<dd>
+<h5><a href="http://www.passingdb.com/" title="Anglické stránky o ¾onglování." class="external" onclick="pageTracker._trackPageview('/goto/passingdb.com');">passingdb.com</a></h5>
+<p>
+<a href="http://www.passingdb.com/" title="Anglické stránky o ¾onglování." onclick="pageTracker._trackPageview('/goto/passingdb.com');"><? echo img("odkazy-passingdb.com.png","passingdb.com"); ?></a>
 Informace o passování.
-</dd>
+</p>
 
-<dt>
-<a href="http://juggling.tv" title="Anglické stránky o ¾onglování." class="external" onclick="pageTracker._trackPageview('/goto/juggling.tv');">juggling.tv</a>
-</dt>
-<dd>
+<h5><a href="http://juggling.tv" title="Anglické stránky o ¾onglování." class="external" onclick="pageTracker._trackPageview('/goto/juggling.tv');">juggling.tv</a></h5>
+<p>
+<a href="http://juggling.tv" title="Anglické stránky o ¾onglování." onclick="pageTracker._trackPageview('/goto/juggling.tv');"><? echo img("odkazy-juggling.tv.png","juggling.tv"); ?></a>
 ®onglérská televize.
-</dd>
+</p>
 
-<dt>
-<a href="http://juggletube.com" title="Anglické stránky o ¾onglování." class="external" onclick="pageTracker._trackPageview('/goto/juggletube.com">juggletube.com</a>
-</dt>
-<dd>
+<h5><a href="http://juggletube.com" title="Anglické stránky o ¾onglování." class="external" onclick="pageTracker._trackPageview('/goto/juggletube.com">juggletube.com</a></h5>
+<p>
+<a href="http://juggletube.com" title="Anglické stránky o ¾onglování." onclick="pageTracker._trackPageview('/goto/juggletube.com"><? echo img("odkazy-juggletube.com.png","juggletube.com"); ?></a>
 Stránka plná ¾onglérského videa.
-</dd>
+</p>
 
 
-<dt>
-<a href="http://www.twjc.co.uk/tutorials.html" title="Anglické stránky o ¾onglování." class="external" onclick="pageTracker._trackPageview('/goto/twjc.co.uk');">Tunbridge Wells Juggling Club Tutorials</a>
-</dt>
-<dd>
+<h5><a href="http://www.twjc.co.uk/tutorials.html" title="Anglické stránky o ¾onglování." class="external" onclick="pageTracker._trackPageview('/goto/twjc.co.uk');">Tunbridge Wells Juggling Club Tutorials</a></h5>
+<p>
+<a href="http://www.twjc.co.uk/tutorials.html" title="Anglické stránky o ¾onglování." onclick="pageTracker._trackPageview('/goto/twjc.co.uk');"><? echo img("odkazy-twjc.png","twjc.co.uk"); ?></a>
 Textové návody na ¾onglování.
-</dd>
+</p>
 
-</dl>
 
 <p>
 Spousta pìkných stránek o ¾onglování je v angliètinì. Mo¾ná se ti bude hodit <a href="aczslovnicek.html" title="Anglicko-èeský ¾onglérský slovníèek.">malý slovníèek</a>.
