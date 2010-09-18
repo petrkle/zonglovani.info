86d3347cb2424f2f08f5ab8b65191974efedd742
Petr <petr@kle.cz>
2010-09-18 08:28:29 +0000

    Datumy v odděleném souboru

---
+++ ulita/datumy.php
@@ -0,0 +1,37 @@
+<?php
+
+$podzim=array();
+$jaro=array();
+
+array_push($podzim,array('datum'=>'2010/10/3','url'=>'udalost-20101003-20101003-pek-1284638162.html'));
+array_push($podzim,array('datum'=>'2010/10/17','url'=>'udalost-20101017-20101017-pek-1284638265.html'));
+array_push($podzim,array('datum'=>'2010/10/31','url'=>'udalost-20101031-20101031-pek-1284638445.html'));
+array_push($podzim,array('datum'=>'2010/11/14','url'=>'udalost-20101114-20101114-pek-1284638655.html'));
+array_push($podzim,array('datum'=>'2010/11/21','url'=>'udalost-20101121-20101121-pek-1284638694.html'));
+array_push($podzim,array('datum'=>'2010/12/5','url'=>'udalost-20101205-20101205-pek-1284638791.html'));
+array_push($podzim,array('datum'=>'2010/12/19','url'=>'udalost-20101219-20101219-pek-1284638839.html'));
+
+array_push($jaro,array('datum'=>'2011/1/16','url'=>'udalost-20110116-20110116-pek-1284720961.html'));
+array_push($jaro,array('datum'=>'2011/1/30','url'=>'udalost-20110130-20110130-pek-1284720999.html'));
+array_push($jaro,array('datum'=>'2011/2/13','url'=>'udalost-20110213-20110213-pek-1284721119.html'));
+array_push($jaro,array('datum'=>'2011/2/27','url'=>'udalost-20110227-20110227-pek-1284721165.html'));
+array_push($jaro,array('datum'=>'2011/3/6','url'=>'udalost-20110306-20110306-pek-1284721221.html'));
+array_push($jaro,array('datum'=>'2011/3/20','url'=>'udalost-20110320-20110320-pek-1284721302.html'));
+array_push($jaro,array('datum'=>'2011/4/3','url'=>'udalost-20110403-20110403-pek-1284721347.html'));
+array_push($jaro,array('datum'=>'2011/4/17','url'=>'udalost-20110417-20110417-pek-1284721417.html'));
+
+function to_ulita($datumy){
+	$zacatekakce=' 16:00:00';
+	$konecakce=' 19:00:00';
+	$navrat=array();
+	foreach($datumy as $foo){
+		$datum=strtotime($foo['datum'].$konecakce);
+		$zdatum=strtotime($foo['datum'].$zacatekakce);
+		if($datum>time()){
+			array_push($navrat,array('datum'=>date('j. n.',$datum),'url'=>$foo['url'],'mz'=>date('c',$datum),'mk'=>date('c',$zdatum)));
+		}
+	}
+	return $navrat;
+}
+
+?>
--- ulita/en.php
+++ ulita/en.php
@@ -1,29 +1,9 @@
 <?php
 require('../init.php');
 require('../func.php');
+require('datumy.php');
 
 $smarty->assign('titulek','Juggling at Ulita');
-
-$podzim=array();
-$jaro=array();
-
-
-array_push($podzim,array('datum'=>'2010/10/3','url'=>'udalost-20101003-20101003-pek-1284638162.html'));
-array_push($podzim,array('datum'=>'2010/10/17','url'=>'udalost-20101017-20101017-pek-1284638265.html'));
-array_push($podzim,array('datum'=>'2010/10/31','url'=>'udalost-20101031-20101031-pek-1284638445.html'));
-array_push($podzim,array('datum'=>'2010/11/14','url'=>'udalost-20101114-20101114-pek-1284638655.html'));
-array_push($podzim,array('datum'=>'2010/11/21','url'=>'udalost-20101121-20101121-pek-1284638694.html'));
-array_push($podzim,array('datum'=>'2010/12/5','url'=>'udalost-20101205-20101205-pek-1284638791.html'));
-array_push($podzim,array('datum'=>'2010/12/19','url'=>'udalost-20101219-20101219-pek-1284638839.html'));
-
-array_push($jaro,array('datum'=>'2010/1/10','url'=>''));
-array_push($jaro,array('datum'=>'2010/1/24','url'=>''));
-array_push($jaro,array('datum'=>'2010/2/7','url'=>''));
-array_push($jaro,array('datum'=>'2010/2/21','url'=>''));
-array_push($jaro,array('datum'=>'2010/3/7','url'=>''));
-array_push($jaro,array('datum'=>'2010/3/21','url'=>''));
-array_push($jaro,array('datum'=>'2010/4/4','url'=>''));
-array_push($jaro,array('datum'=>'2010/4/11','url'=>''));
 
 $smarty->assign('podzim',to_ulita($podzim));
 $smarty->assign('jaro',to_ulita($jaro));
@@ -36,18 +16,5 @@ $smarty->assign_by_ref('trail', $trail->path);
 $smarty->display('hlavicka.tpl');
 $smarty->display('ulita.en.tpl');
 $smarty->display('paticka.tpl');
-
-
-function to_ulita($datumy){
-	$konecakce=" 19:00:00";
-	$navrat=array();
-	foreach($datumy as $foo){
-		$datum=strtotime($foo['datum'].$konecakce);
-		if($datum>time()){
-			array_push($navrat,array('datum'=>date("j. n.",$datum),'url'=>$foo['url']));
-		}
-	}
-	return $navrat;
-}
 
 ?>
