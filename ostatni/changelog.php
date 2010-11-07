c1dbaa83748250a20847309c786376fa976d3e82
Petr <petr@kle.cz>
2010-11-07 12:39:56 +0100

    Stránkovaný seznam změn.

--- ostatni/changelog.php
+++ ostatni/changelog.php
@@ -1,12 +1,8 @@
 <?php
 require('../init.php');
 require('../func.php');
+require($lib.'/Pager/Pager.php');
 
-$titulek='Změny v žonglérově slabikáři';
-$smarty->assign('keywords','změny, žonglérův slabikář, changelog');
-$smarty->assign('description','Seznam změn v žonglérově slabikáři.');
-$smarty->assign('feedback',true);
-$smarty->assign('titulek',$titulek);
 
 $dalsi=array(
 	array('url'=>'/proc-a-jak.html','text'=>'Proč a jak vznikl žonglérův slabikář','title'=>'Proč a jak vznikl žonglérův slabikář'),
@@ -16,13 +12,59 @@ $dalsi=array(
 	);
 $smarty->assign_by_ref('dalsi',$dalsi);
 
+$zmeny=get_changelog();
+
+$pagerOptions = array(
+    'mode'     => 'Sliding',
+    'delta'    => 2,
+		'firstLinkTitle' => 'První stránka',
+    'perPage'  => 25,
+    'altPrev'  => 'Předchozí stránka',
+    'altNext'  => 'Další stránka',
+    'altPage'  => 'Stránka',
+    'separator'  => '~',
+    'spacesBeforeSeparator'  => 1,
+    'spacesAfterSeparator'  => 1,
+		'append'   => false,
+		'path'		=> '/',
+		'fileName' => 'changelog-%d.html', 
+    'itemData' => $zmeny,
+);
+$pager =& Pager::factory($pagerOptions);
+$data = $pager->getPageData();
+
+$smarty->assign(
+    'page_numbers', array(
+        'current' => $pager->getCurrentPageID(),
+        'total'   => $pager->numPages()
+    )
+);
+
+$titulek='Změny v žonglérově slabikáři';
+$nadpis=$titulek;
+$popis='Seznam změn v žonglérově slabikáři.';
+
 $trail = new Trail();
-$trail->addStep($titulek);
+$trail->addStep($titulek,'/changelog.html');
+
+if($pager->getCurrentPageID()>1){
+	$trail->addStep($pager->getCurrentPageID().'. stránka','/tip/archiv'.$pager->getCurrentPageID().'.html');
+	$titulek.=' '.$pager->getCurrentPageID().'. stránka';
+	$popis.=' '.$pager->getCurrentPageID().'. stránka';
+}
+
+$smarty->assign('nadpis',$nadpis);
+$smarty->assign('keywords','změny, žonglérův slabikář, changelog');
+$smarty->assign('description',$popis);
+$smarty->assign('feedback',true);
+$smarty->assign('titulek',$titulek);
 
 $smarty->assign_by_ref('trail', $trail->path);
 $smarty->display('hlavicka.tpl');
 
-$smarty->assign('zmeny',get_changelog());
+$smarty->assign('pager_links', $pager->links);
+
+$smarty->assign('zmeny',$data);
 $smarty->display('ostatni-changelog.tpl');
 
 $smarty->display('paticka.tpl');
--- templates/ostatni-changelog.tpl
+++ templates/ostatni-changelog.tpl
@@ -7,10 +7,16 @@
 </ul>
 {/foreach}
 {/if}
-
+{if $page_numbers.total == $page_numbers.current}
 <h3>Změny ve starším uložišti svn</h3>
 <pre>{include file='ostatni-changelog-old.tpl'}</pre>
 <h3>Starší změny</h3>
 <ul>
 <li>jaro 2005 - <a href="/proc-a-jak.html" title="Proč a jak vznikl žonglérův slabikář">první verze</a> žonglérova slabikáře</li>
 </ul>
+{/if}
+{if $page_numbers.total > 1}
+<p>
+Stránkování: {$pager_links}
+</p>
+{/if}
