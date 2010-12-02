d835127eadcf904163a8b0efe81fd2cd696b13e2
Petr <petr@kle.cz>
2010-12-02 20:41:53 +0100

    Vánoční obrázek

--- ostatni/vanoce-2009.php
+++ ostatni/vanoce-2009.php
@@ -2,15 +2,15 @@
 require('../init.php');
 require('../func.php');
 $titulek='Veselé vánoce';
-$smarty->assign('titulek',$titulek.' 2010');
+$smarty->assign('titulek',$titulek.' 2009');
 $smarty->assign('nadpis',$titulek);
 $smarty->assign('keywords',make_keywords($titulek).', žonglování');
-$smarty->assign('description','Veselé vánoce 2010 všem žonglérkám a žonglérům.');
+$smarty->assign('description','Veselé vánoce 2009 všem žonglérkám a žonglérům.');
 $trail = new Trail();
 $trail->addStep('Tip týdne','/tip');
 $trail->addStep($titulek);
 $smarty->assign_by_ref('trail', $trail->path);
 $smarty->display('hlavicka.tpl');
-$smarty->display('ostatni-vanoce-2010.tpl');
+$smarty->display('ostatni-vanoce-2009.tpl');
 $smarty->display('paticka.tpl');
 ?>
---
+++ templates/ostatni-vanoce-2010.tpl
@@ -0,0 +1,5 @@
+<p>
+<a href="/img/v/vanoce-2010.jpg" title="Žonglérský svícen">{obrazek soubor='vanoce-2010.s.jpg' popisek='Žonglérský svícen'}</a>
+Veselé vánoční svátky. Ať vám to lítá :^)<br />
+<a href="/kontakt.html" title="Petr Kletečka">Petr</a>
+</p>
