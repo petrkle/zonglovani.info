d430956f09f1d579ade1b460d47d180cfd35a099
Petr <petr@kle.cz>
2011-06-19 22:24:58 +0200

    Html 5 test

---
+++ html5/index.php
@@ -0,0 +1,10 @@
+<html>
+	<head>
+		<title>Video test</title>
+	</head>
+	<body>
+		<video src="multi-columns.webm" controls="controls" autoplay="autoplay" loop="loop">
+		your browser does not support the video tag
+		</video>
+	</body>
+</html>
