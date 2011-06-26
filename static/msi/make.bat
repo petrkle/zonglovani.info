d2504cbe8e75376ee90b2826b4dddc350db21033
Petr <petr@kle.cz>
2011-06-26 19:14:39 +0200

    Verze 0.3 ke stažení - msi, exe.

--- static/msi/make.bat
+++
@@ -1,4 +0,0 @@
-rem copy /b z:\7z920_extra\7zSD.sfx + zonglovani.conf + zonglovani.7z zongleruv-slabikar.exe
-candle zonglovani.wsx
-light -ext WixUIExtension -cultures:cs-cz zonglovani.wixobj
-zonglovani.msi
