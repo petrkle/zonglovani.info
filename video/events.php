0c2ef0495bf718cbdaced4cd245f4dfeb71cbb3e
Petr <petr@kle.cz>
2011-07-06 16:47:38 +0200

    Video návody na žonglování.

--- video/events.php
+++ video/events.php
@@ -2,6 +2,7 @@
 
 $juggling_events=array(
 	'pzm2'=>'Pražský žonglérský marathon 2',
-	'drazdany-2011'=>'Festival žonglování v Drážďanech'
+	'drazdany-2011'=>'Festival žonglování v Drážďanech',
+	'navody'=>'Video návody na žonglování'
 	);
 
