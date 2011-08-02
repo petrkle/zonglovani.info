10cf2ccb6db368b8f876eafab5a763483a599d8c
Petr <petr@kle.cz>
2011-08-02 23:42:58 +0200

    Křišťálová lupa - odkaz na nominaci

---
+++ ostatni/krlupa.php
@@ -0,0 +1,20 @@
+<?php
+require('../init.php');
+require('../func.php');
+$titulek='Křišťálová lupa';
+$smarty->assign('keywords',make_keywords($titulek).', žonglování, nominace');
+$smarty->assign('description','Nominujte žonglérův slabikář do Křišťálové lupy 2011.');
+$smarty->assign('titulek',$titulek);
+$smarty->assign('nahled','http://'.$_SERVER['SERVER_NAME'].'/img/l/lupa2011.png');
+$trail = new Trail();
+$trail->addStep($titulek);
+$dalsi=array(
+	array('url'=>'http://kristalova.lupa.cz/nominace/','text'=>'Formulář pro nominaci','title'=>'Formulář pro nominaci žonglérova slabikáře do Křišťálové lupy.'),
+	array('url'=>'/podporte-zongleruv-slabikar.html','text'=>'Další možnosti podpory žonglérova slabikáře','title'=>'Vložení odkazu na web a tak.')
+	);
+$smarty->assign_by_ref('dalsi',$dalsi);
+$smarty->assign_by_ref('trail', $trail->path);
+$smarty->display('hlavicka.tpl');
+$smarty->display('ostatni-krlupa.tpl');
+$smarty->display('paticka.tpl');
+?>
---
+++ templates/ostatni-krlupa.tpl
@@ -0,0 +1,11 @@
+<p>
+{obrazek soubor='lupa2011.png' popisek='Křišťálová lupa 2011'}
+Křišťálová lupa je anketa o nejlepší weby českého internetu.
+</p>
+
+<p><a href="http://kristalova.lupa.cz/nominace/" title="Formulář pro nominaci" class="external" onclick="_gaq.push(['_trackPageview','/goto/kristalova.lupa.cz/nominace']);" rel="nofollow">Nominujte</a> žonglérův slabikář.</p>
+
+<p>
+<a href="http://kristalova.lupa.cz/nominace/" title="Formulář pro nominaci" onclick="_gaq.push(['_trackPageview','/goto/kristalova.lupa.cz/nominace']);" rel="nofollow">{obrazek soubor='lupaa.png' popisek='Správně vyplněný formulář'}</a><br style="clear:both" />
+Příklad vyplnění formuláře.
+</p>
