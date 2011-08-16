0f786a9783319d781b37f6745eda24aaf4b2ca21
Petr <petr@kle.cz>
2011-08-16 21:37:23 +0200

    Odstranění videa Solstix.

--- video/events.php
+++ video/events.php
@@ -1,10 +1,10 @@
 <?php
 
 $juggling_events=array(
-	'pzm2'=>'Pražský žonglérský marathon 2',
-	'drazdany-2011'=>'Festival žonglování v Drážďanech',
+#	'pzm2'=>'Pražský žonglérský marathon 2',
+#	'drazdany-2011'=>'Festival žonglování v Drážďanech',
 	'navody'=>'Video návody na žonglování',
 	'metlili'=>'Metlili',
-	'ejc'=>'EJC',
+#	'ejc'=>'EJC',
 	);
 
