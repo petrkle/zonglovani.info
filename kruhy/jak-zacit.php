607b0391d2101b5ef279b901edac07a1a6e6d213
Petr <petr@kle.cz>
2011-10-28 17:33:06 +0200

    Přidání textů do pdf.

--- kruhy/jak-zacit.php
+++ kruhy/jak-zacit.php
@@ -8,20 +8,18 @@ $smarty->assign('feedback',true);
 $smarty->assign('keywords',make_keywords($titulek));
 $smarty->assign('description','Obrázkový návod na žonglování s kruhy');
 
-$dalsi=array(
-	array('url'=>'/kruhy/3/kaskada.html','text'=>'Kaskáda se třemi kruhy','title'=>'Nejjednodušší způsob žonglování'),
-	array('url'=>'/kruhy/vyroba.html','text'=>'Výroba žonglovacích kruhů','title'=>'Jak vyrobit pěkné a levné kruhy na žonglování'),
-	);
-$smarty->assign_by_ref('dalsi',$dalsi);
-
 $trail = new Trail();
 $trail->addStep('Kruhy','/kruhy/');
 $smarty->assign('titulek',$titulek);
 $smarty->assign('keywords',make_keywords($titulek));
 $trail->addStep($titulek);
 $smarty->assign_by_ref('trail', $trail->path);
+
+$trik=nacti_trik('kruhy-jak-zacit');
+$smarty->assign('trik',$trik);
+
 $smarty->display('hlavicka.tpl');
-$smarty->display('kruhy-jak-zacit.tpl');
+$smarty->display('trik.tpl');
 $smarty->display('paticka.tpl');
 
 ?>
--- static/pdf/priprav-obrazky.sh
+++ static/pdf/priprav-obrazky.sh
@@ -11,13 +11,13 @@ do
 	name=`echo $foo | sed "s/\.png$//"`
 	if [ -f "../../$name.fig" ] ; then
 		fig2dev -L png -b 5 -S 4 -m 2 "../../$name.fig" "$name.png"
-		convert -type Grayscale -border 2x2 -bordercolor "#000000" $foo $foo
+		convert -type Grayscale $foo $foo
 	else
-		convert -type Grayscale -border 1x1 -bordercolor "#000000" $foo $foo
+		convert -type Grayscale $foo $foo
 	fi
 done
 
 for foo in `find img -type f -name "*.jpg"`
 do
-	convert -type Grayscale -border 1x1 -bordercolor "#000000" $foo $foo
+	convert -type Grayscale $foo $foo
 done
