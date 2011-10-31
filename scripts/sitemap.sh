e2db42325bf2abdc48f026bab0f7f8d087b5f353
Petr <petr@kle.cz>
2011-10-31 20:36:26 +0100

    Aktualizace itemap.xml a přidružených skriptů.

--- scripts/sitemap.sh
+++ scripts/sitemap.sh
@@ -5,6 +5,8 @@ rm -rf zongl.info/ 2>/dev/null
 ./scripts/stahni.sh
 ./scripts/tree.pl /home/www/zonglovani.info/zongl.info | sed "s/index\.html//g" > /home/www/zonglovani.info/mapa-stranek.inc
 ./scripts/tree-full.pl /home/www/zonglovani.info/zongl.info | sed "s/index\.html//g" > /home/www/zonglovani.info/mapa-stranek.full
+
+sed -i 's/Diskuse a komentáře - .*\. stránka/Diskuse a komentáře/' mapa-stranek.inc
 rm -rf zongl.info/ 2>/dev/null
 ./scripts/sitemap.php > sitemap.xml
 rm -f mapa-stranek.full
