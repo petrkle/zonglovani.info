7fe0253e38be701a8d65dbc4dd656b1ba0c88622
Petr <petr@kle.cz>
2012-03-31 17:09:14 +0200

    Nové tipy týdne

--- tip/pondelky.php
+++ tip/pondelky.php
@@ -1,8 +1,9 @@
+#!/usr/bin/php
 <?php
+date_default_timezone_set('Europe/Prague');
 for($foo=0;$foo<1000;$foo++){
-	$bar=strtotime('4/1/2011')+($foo*24*3600);
+	$bar=strtotime('11/1/2012')+($foo*24*3600);
 	if(date('N',$bar)==1){
-		print date('Y-m-d',$bar).'<br />';
+		print date('Y-m-d',$bar)."\n";
 	}
 }
-?>
