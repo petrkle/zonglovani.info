9ab9037dafff12617b6f28fefe247d68d23f929f
Petr <petr@kle.cz>
2012-10-21 20:09:37 +0200

    Kruhová parabola - obrázky rss.

--- rss/rss.php
+++ rss/rss.php
@@ -2,27 +2,90 @@
 
 $rss_zdroje=array();
 	
-$rss_zdroje['fireshow']=array('popis'=>'fireshow.cz','url'=>'http://fireshow.cz','feed_url'=>'http://feeds.feedburner.com/fireshowcz?format=xml');
-$rss_zdroje['tribo']=array('popis'=>'tribofuego.org','url'=>'http://tribofuego.org','feed_url'=>'http://www.facebook.com/feeds/page.php?format=atom10&id=51726915625');
-$rss_zdroje['infinitos']=array('popis'=>'infinitos.cz','url'=>'http://infinitos.cz','feed_url'=>'http://www.facebook.com/feeds/page.php?format=atom10&id=314458838078');
-$rss_zdroje['lideczongl']=array('popis'=>'Lidé.cz - žonglování','url'=>'http://forum.lide.cz/forum.fcgi?akce=forum_data&auth=&forum_ID=7547','feed_url'=>'http://forum.lide.cz/rss/7547');
-$rss_zdroje['jugglesk']=array('popis'=>'juggle.sk','url'=>'http://juggle.sk','feed_url'=>'http://juggle.sk/feed/');
-$rss_zdroje['jugglesk_forum']=array('popis'=>'juggle.sk - forum','url'=>'http://juggle.sk/sk/forum','feed_url'=>'http://juggle.sk/wp-content/plugins/mingle-forum/feed.php?topic=all');
-$rss_zdroje['zonglujeu']=array('popis'=>'zongluj.eu','url'=>'http://zongluj.eu','feed_url'=>'http://www.zongluj.eu/rss-kniha/');
-$rss_zdroje['flow']=array('popis'=>'The Flow','url'=>'http://www.theflow.cz','feed_url'=>'http://www.theflow.cz/frontpage/rss.html');
-$rss_zdroje['zonglujcz']=array('popis'=>'zongluj.cz','url'=>'http://zongluj.cz','feed_url'=>'http://www.facebook.com/feeds/page.php?format=atom10&id=283517950634');
-$rss_zdroje['bratri']=array('popis'=>'Bratři v tricku','url'=>'http://bratrivtricku.cz','feed_url'=>'http://bratrivtricku.cz/feed');
-$rss_zdroje['cirqueon']=array('popis'=>'cirqueon.cz','url'=>'http://www.cirqueon.cz','feed_url'=>'http://www.facebook.com/feeds/page.php?format=atom10&id=158223121757');
-$rss_zdroje['dvaadvacitka']=array('popis'=>'22.cz','url'=>'http://www.22.cz','feed_url'=>'http://www.22.cz/blog/rss/');
-#$rss_zdroje['kruhovaparabola']=array('popis'=>'Kruhová parabola - kniha návštěv','url'=>'http://kruhovaparabola.cz','feed_url'=>'http://blueboard.cz/rss-knihy.php?id=189532');
-$rss_zdroje['zongler']=array('popis'=>'zongler.cz','url'=>'http://zongler.cz','feed_url'=>'http://www.zongler.cz/index2.php?option=com_rss&no_html=1');
-$rss_zdroje['adinfinitum']=array('popis'=>'Ad infinitum - fotky','url'=>'http://www.adinfinitum.cz/','feed_url'=>'http://ad8.rajce.idnes.cz/?rss=news');
-$rss_zdroje['esteladefuego']=array('popis'=>'Estela de Fuego','url'=>'http://www.esteladefuego.cz/','feed_url'=>'http://www.esteladefuego.cz/feed/');
-$rss_zdroje['slabikarkalendar']=array('popis'=>'Žonglérův slabikář - kalendář','url'=>'http://zonglovani.info/kalendar/','feed_url'=>'http://zonglovani.info/kalendar/kalendar.xml');
-$rss_zdroje['slabikarlide']=array('popis'=>'Seznam žonglérů','url'=>'http://zonglovani.info/lide/','feed_url'=>'http://zonglovani.info/lide/uzivatele.xml');
-$rss_zdroje['slabikardiskuse']=array('popis'=>'Žonglérův slabikář - diskuse','url'=>'http://zonglovani.info/diskuse/','feed_url'=>'http://zonglovani.info/diskuse/zpravy.xml');
-$rss_zdroje['slabikartip']=array('popis'=>'Žonglérův slabikář - tip týdne','url'=>'http://zonglovani.info/tip/','feed_url'=>'http://zonglovani.info/tip/tip.xml');
-$rss_zdroje['slabikarobrazky']=array('popis'=>'Žonglérův slabikář - obrázky','url'=>'http://zonglovani.info/obrazky/','feed_url'=>'http://zonglovani.info/obrazky/obrazky.xml');
+$rss_zdroje['fireshow']=array(
+	'popis'=>'fireshow.cz',
+	'url'=>'http://fireshow.cz',
+	'feed_url'=>'http://feeds.feedburner.com/fireshowcz?format=xml');
+$rss_zdroje['tribo']=array(
+	'popis'=>'tribofuego.org',
+	'url'=>'http://tribofuego.org',
+	'feed_url'=>'http://www.facebook.com/feeds/page.php?format=atom10&id=51726915625');
+$rss_zdroje['infinitos']=array(
+	'popis'=>'infinitos.cz',
+	'url'=>'http://infinitos.cz',
+	'feed_url'=>'http://www.facebook.com/feeds/page.php?format=atom10&id=314458838078');
+$rss_zdroje['lideczongl']=array(
+	'popis'=>'Lidé.cz - žonglování',
+	'url'=>'http://forum.lide.cz/forum.fcgi?akce=forum_data&auth=&forum_ID=7547',
+	'feed_url'=>'http://forum.lide.cz/rss/7547');
+$rss_zdroje['jugglesk']=array(
+	'popis'=>'juggle.sk',
+	'url'=>'http://juggle.sk',
+	'feed_url'=>'http://juggle.sk/feed/');
+$rss_zdroje['jugglesk_forum']=array(
+	'popis'=>'juggle.sk - forum',
+	'url'=>'http://juggle.sk/sk/forum',
+	'feed_url'=>'http://juggle.sk/wp-content/plugins/mingle-forum/feed.php?topic=all');
+$rss_zdroje['zonglujeu']=array(
+	'popis'=>'zongluj.eu',
+	'url'=>'http://zongluj.eu',
+	'feed_url'=>'http://www.zongluj.eu/rss-kniha/');
+$rss_zdroje['flow']=array(
+	'popis'=>'The Flow',
+	'url'=>'http://www.theflow.cz',
+	'feed_url'=>'http://www.theflow.cz/frontpage/rss.html');
+$rss_zdroje['zonglujcz']=array(
+	'popis'=>'zongluj.cz',
+	'url'=>'http://zongluj.cz',
+	'feed_url'=>'http://www.facebook.com/feeds/page.php?format=atom10&id=283517950634');
+$rss_zdroje['bratri']=array(
+	'popis'=>'Bratři v tricku',
+	'url'=>'http://bratrivtricku.cz',
+	'feed_url'=>'http://bratrivtricku.cz/feed');
+$rss_zdroje['cirqueon']=array(
+	'popis'=>'cirqueon.cz',
+	'url'=>'http://www.cirqueon.cz',
+	'feed_url'=>'http://www.facebook.com/feeds/page.php?format=atom10&id=158223121757');
+$rss_zdroje['dvaadvacitka']=array(
+	'popis'=>'22.cz',
+	'url'=>'http://www.22.cz',
+	'feed_url'=>'http://www.22.cz/blog/rss/');
+$rss_zdroje['kruhovaparabola']=array(
+	'popis'=>'Kruhová parabola - obrázky',
+	'url'=>'http://zongleri.cz',
+	'feed_url'=>'http://kruhovaparabola.rajce.idnes.cz/?rss=news');
+$rss_zdroje['zongler']=array(
+	'popis'=>'zongler.cz',
+	'url'=>'http://zongler.cz',
+	'feed_url'=>'http://www.zongler.cz/index2.php?option=com_rss&no_html=1');
+$rss_zdroje['adinfinitum']=array(
+	'popis'=>'Ad infinitum - fotky',
+	'url'=>'http://www.adinfinitum.cz/',
+	'feed_url'=>'http://ad8.rajce.idnes.cz/?rss=news');
+$rss_zdroje['esteladefuego']=array(
+	'popis'=>'Estela de Fuego',
+	'url'=>'http://www.esteladefuego.cz/',
+	'feed_url'=>'http://www.esteladefuego.cz/feed/');
+$rss_zdroje['slabikarkalendar']=array(
+	'popis'=>'Žonglérův slabikář - kalendář',
+	'url'=>'http://zonglovani.info/kalendar/',
+	'feed_url'=>'http://zonglovani.info/kalendar/kalendar.xml');
+$rss_zdroje['slabikarlide']=array(
+	'popis'=>'Seznam žonglérů',
+	'url'=>'http://zonglovani.info/lide/',
+	'feed_url'=>'http://zonglovani.info/lide/uzivatele.xml');
+$rss_zdroje['slabikardiskuse']=array(
+	'popis'=>'Žonglérův slabikář - diskuse',
+	'url'=>'http://zonglovani.info/diskuse/',
+	'feed_url'=>'http://zonglovani.info/diskuse/zpravy.xml');
+$rss_zdroje['slabikartip']=array(
+	'popis'=>'Žonglérův slabikář - tip týdne',
+	'url'=>'http://zonglovani.info/tip/',
+	'feed_url'=>'http://zonglovani.info/tip/tip.xml');
+$rss_zdroje['slabikarobrazky']=array(
+	'popis'=>'Žonglérův slabikář - obrázky',
+	'url'=>'http://zonglovani.info/obrazky/',
+	'feed_url'=>'http://zonglovani.info/obrazky/obrazky.xml');
 	
 function get_news($pocet,$filtr=false){
 	global $rss_zdroje;
@@ -64,4 +127,3 @@ function get_news($pocet,$filtr=false){
 	}
 	return $navrat;
 }
-?>
