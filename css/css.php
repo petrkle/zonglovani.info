319375d7bc9395b153a3f8178ddd23660df0bd36
Petr <petr@kle.cz>
2012-11-23 21:33:13 +0100

    Hlavičky expire, last-modified

--- css/css.php
+++ css/css.php
@@ -4,6 +4,7 @@ header ('content-type: text/css; charset: UTF-8');
 $offset = 7 * 24 * 3600;
 $expire = 'expires: ' . gmdate ('D, d M Y H:i:s', time() + $offset) . ' GMT';
 header ($expire);
+header("Last-Modified: ".gmdate("D, d M Y H:i:s", filemtime('./'.$_GET['style'].'.css'))." GMT");
 if (substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip')){
 	ob_start('ob_gzhandler');
 }
