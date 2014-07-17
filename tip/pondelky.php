36f7298096b04bf448aad1685b170e677eb742f2
Petr <petr@kle.cz>
2014-07-17 21:21:18 +0200

    Nové tipy týdne

--- tip/pondelky.php
+++ tip/pondelky.php
@@ -1,8 +1,8 @@
 #!/usr/bin/php
 <?php
 date_default_timezone_set('Europe/Prague');
-for($foo=0;$foo<400;$foo++){
-	$bar=strtotime('12/30/2013')+($foo*24*3600);
+for($foo=0;$foo<800;$foo++){
+	$bar=strtotime('07/08/2014')+($foo*24*3600);
 	if(date('N',$bar)==1){
 		print date('Y-m-d',$bar)."\n";
 	}
---
+++ tip/tip.js
@@ -0,0 +1,14 @@
+var casper = require('casper').create({
+	verbose: false,
+	logLevel: 'info',
+  pageSettings: { loadImages: true, loadPlugins: false	}
+});
+
+casper.start('http://zongl.info' + casper.cli.get('url'));
+
+casper.then(function() {
+
+		this.echo(this.fetchText('h1') + '*' + casper.cli.get('url') + '*' + this.getElementAttribute('#obsah img:nth-of-type(1)', 'src').split('/').reverse()[0] + '*' + this.fetchText('#obsah p:nth-of-type(2)').replace(/(\r\n|\n|\r)/gm,"") );
+});
+
+casper.run();
