ef86847d9270c2dfe07f956cb170b8b994bd2d85
Petr <petr@kle.cz>
2014-07-17 21:25:35 +0200

    Přidání ISBN do pdf verze.

--- static/pdf/pdf.sh
+++ static/pdf/pdf.sh
@@ -11,35 +11,13 @@ else
 	VERSION=$1
 fi
 
-for foo in `cat url.3`
+for baz in url.*
 do
-	./xml2tex.php $foo
-done > tri-micky.tex
-
-for foo in `cat url.4`
-do
-	./xml2tex.php $foo
-done > ctyri-micky.tex
-
-for foo in `cat url.5`
-do
-	./xml2tex.php $foo
-done > pet-micku.tex
-
-for foo in `cat url.kruhy`
-do
-	./xml2tex.php $foo
-done > kruhy.tex
-
-for foo in `cat url.kuzely`
-do
-	./xml2tex.php $foo
-done > kuzely.tex
-
-for foo in `cat url.passing`
-do
-	./xml2tex.php $foo
-done > passing.tex
+	for foo in `cat $baz`
+	do
+		./xml2tex.php $foo
+	done > `echo $baz | sed "s/.*url\.//"`.tex
+done
 
 cp sablona.tex zongleruv-slabikar-$VERSION.tex
 
