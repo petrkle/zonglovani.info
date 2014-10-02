184e973f20b973cc417d50ef889941dbdfbdd139
Petr <petr@kle.cz>
2014-10-02 20:31:48 +0200

    Oprava linků - Firefox OS

--- static/mobile/stahni.sh
+++ static/mobile/stahni.sh
@@ -14,7 +14,7 @@ wget \
 	http://zongl.info/ \
 	http://zongl.info/plain-$CSS_CHKSUM.css \
 	http://zongl.info/img/e/external.png \
-	--reject 'pdf,juggling-tv.html,doplnky-prohlizece.html,odkazy.html,mobil.html,exkurze.html,vanoce-*.html,pf-*.html,changelog*.html,statistiky.html,odkazy.html,navody.html' \
+	--reject 'pdf,juggling-tv.html,doplnky-prohlizece.html,odkazy.html,mobil.html,exkurze.html,vanoce-*.html,pf-*.html,changelog*.html,statistiky.html,odkazy.html,navody.html,changelog.html,rss.html,tip.html,jak-odkazovat.html' \
 	-X animace,video,obrazky,mapa,kalendar,ulita,lide,diskuse,obrazky-na-plochu,navody,scripts,horoskop,vyhledavani,tip,rss,download,olympiada,mdz,g,css
 
 rm -rf zongleruv-slabikar
