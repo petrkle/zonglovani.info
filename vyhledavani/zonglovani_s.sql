bc344ca33fee84fb02b6bc37191557a7b394098d
Petr <petr@kle.cz>
2015-02-26 17:56:23 +0100

    Šablona db pro vyhledávání

--- vyhledavani/zonglovani_s.sql
+++ vyhledavani/zonglovani_s.sql
@@ -7,6 +7,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 
+DROP TABLE IF EXISTS `search_categories`;
 CREATE TABLE IF NOT EXISTS `search_categories` (
   `category_id` int(11) NOT NULL AUTO_INCREMENT,
   `category` text,
@@ -14,20 +15,26 @@ CREATE TABLE IF NOT EXISTS `search_categories` (
   PRIMARY KEY (`category_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+DROP TABLE IF EXISTS `search_domains`;
 CREATE TABLE IF NOT EXISTS `search_domains` (
   `domain_id` int(11) NOT NULL AUTO_INCREMENT,
   `domain` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`domain_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
+INSERT INTO `search_domains` (`domain_id`, `domain`) VALUES
+(1, 'zongl.info');
+
+DROP TABLE IF EXISTS `search_keywords`;
 CREATE TABLE IF NOT EXISTS `search_keywords` (
   `keyword_id` int(11) NOT NULL AUTO_INCREMENT,
   `keyword` varchar(30) NOT NULL,
   PRIMARY KEY (`keyword_id`),
   UNIQUE KEY `kw` (`keyword`),
   KEY `keyword` (`keyword`(10))
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11078 ;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+DROP TABLE IF EXISTS `search_links`;
 CREATE TABLE IF NOT EXISTS `search_links` (
   `link_id` int(11) NOT NULL AUTO_INCREMENT,
   `site_id` int(11) DEFAULT NULL,
@@ -44,8 +51,9 @@ CREATE TABLE IF NOT EXISTS `search_links` (
   PRIMARY KEY (`link_id`),
   KEY `url` (`url`),
   KEY `md5key` (`md5sum`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2055 ;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+DROP TABLE IF EXISTS `search_link_keyword0`;
 CREATE TABLE IF NOT EXISTS `search_link_keyword0` (
   `link_id` int(11) NOT NULL,
   `keyword_id` int(11) NOT NULL,
@@ -55,6 +63,7 @@ CREATE TABLE IF NOT EXISTS `search_link_keyword0` (
   KEY `keyid` (`keyword_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+DROP TABLE IF EXISTS `search_link_keyword1`;
 CREATE TABLE IF NOT EXISTS `search_link_keyword1` (
   `link_id` int(11) NOT NULL,
   `keyword_id` int(11) NOT NULL,
@@ -64,6 +73,7 @@ CREATE TABLE IF NOT EXISTS `search_link_keyword1` (
   KEY `keyid` (`keyword_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+DROP TABLE IF EXISTS `search_link_keyword2`;
 CREATE TABLE IF NOT EXISTS `search_link_keyword2` (
   `link_id` int(11) NOT NULL,
   `keyword_id` int(11) NOT NULL,
@@ -73,6 +83,7 @@ CREATE TABLE IF NOT EXISTS `search_link_keyword2` (
   KEY `keyid` (`keyword_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+DROP TABLE IF EXISTS `search_link_keyword3`;
 CREATE TABLE IF NOT EXISTS `search_link_keyword3` (
   `link_id` int(11) NOT NULL,
   `keyword_id` int(11) NOT NULL,
@@ -82,6 +93,7 @@ CREATE TABLE IF NOT EXISTS `search_link_keyword3` (
   KEY `keyid` (`keyword_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+DROP TABLE IF EXISTS `search_link_keyword4`;
 CREATE TABLE IF NOT EXISTS `search_link_keyword4` (
   `link_id` int(11) NOT NULL,
   `keyword_id` int(11) NOT NULL,
@@ -91,6 +103,7 @@ CREATE TABLE IF NOT EXISTS `search_link_keyword4` (
   KEY `keyid` (`keyword_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+DROP TABLE IF EXISTS `search_link_keyword5`;
 CREATE TABLE IF NOT EXISTS `search_link_keyword5` (
   `link_id` int(11) NOT NULL,
   `keyword_id` int(11) NOT NULL,
@@ -100,6 +113,7 @@ CREATE TABLE IF NOT EXISTS `search_link_keyword5` (
   KEY `keyid` (`keyword_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+DROP TABLE IF EXISTS `search_link_keyword6`;
 CREATE TABLE IF NOT EXISTS `search_link_keyword6` (
   `link_id` int(11) NOT NULL,
   `keyword_id` int(11) NOT NULL,
@@ -109,6 +123,7 @@ CREATE TABLE IF NOT EXISTS `search_link_keyword6` (
   KEY `keyid` (`keyword_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+DROP TABLE IF EXISTS `search_link_keyword7`;
 CREATE TABLE IF NOT EXISTS `search_link_keyword7` (
   `link_id` int(11) NOT NULL,
   `keyword_id` int(11) NOT NULL,
@@ -118,6 +133,7 @@ CREATE TABLE IF NOT EXISTS `search_link_keyword7` (
   KEY `keyid` (`keyword_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+DROP TABLE IF EXISTS `search_link_keyword8`;
 CREATE TABLE IF NOT EXISTS `search_link_keyword8` (
   `link_id` int(11) NOT NULL,
   `keyword_id` int(11) NOT NULL,
@@ -127,6 +143,7 @@ CREATE TABLE IF NOT EXISTS `search_link_keyword8` (
   KEY `keyid` (`keyword_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+DROP TABLE IF EXISTS `search_link_keyword9`;
 CREATE TABLE IF NOT EXISTS `search_link_keyword9` (
   `link_id` int(11) NOT NULL,
   `keyword_id` int(11) NOT NULL,
@@ -136,6 +153,7 @@ CREATE TABLE IF NOT EXISTS `search_link_keyword9` (
   KEY `keyid` (`keyword_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+DROP TABLE IF EXISTS `search_link_keyworda`;
 CREATE TABLE IF NOT EXISTS `search_link_keyworda` (
   `link_id` int(11) NOT NULL,
   `keyword_id` int(11) NOT NULL,
@@ -145,6 +163,7 @@ CREATE TABLE IF NOT EXISTS `search_link_keyworda` (
   KEY `keyid` (`keyword_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+DROP TABLE IF EXISTS `search_link_keywordb`;
 CREATE TABLE IF NOT EXISTS `search_link_keywordb` (
   `link_id` int(11) NOT NULL,
   `keyword_id` int(11) NOT NULL,
@@ -154,6 +173,7 @@ CREATE TABLE IF NOT EXISTS `search_link_keywordb` (
   KEY `keyid` (`keyword_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+DROP TABLE IF EXISTS `search_link_keywordc`;
 CREATE TABLE IF NOT EXISTS `search_link_keywordc` (
   `link_id` int(11) NOT NULL,
   `keyword_id` int(11) NOT NULL,
@@ -163,6 +183,7 @@ CREATE TABLE IF NOT EXISTS `search_link_keywordc` (
   KEY `keyid` (`keyword_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+DROP TABLE IF EXISTS `search_link_keywordd`;
 CREATE TABLE IF NOT EXISTS `search_link_keywordd` (
   `link_id` int(11) NOT NULL,
   `keyword_id` int(11) NOT NULL,
@@ -172,6 +193,7 @@ CREATE TABLE IF NOT EXISTS `search_link_keywordd` (
   KEY `keyid` (`keyword_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+DROP TABLE IF EXISTS `search_link_keyworde`;
 CREATE TABLE IF NOT EXISTS `search_link_keyworde` (
   `link_id` int(11) NOT NULL,
   `keyword_id` int(11) NOT NULL,
@@ -181,6 +203,7 @@ CREATE TABLE IF NOT EXISTS `search_link_keyworde` (
   KEY `keyid` (`keyword_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+DROP TABLE IF EXISTS `search_link_keywordf`;
 CREATE TABLE IF NOT EXISTS `search_link_keywordf` (
   `link_id` int(11) NOT NULL,
   `keyword_id` int(11) NOT NULL,
@@ -190,6 +213,7 @@ CREATE TABLE IF NOT EXISTS `search_link_keywordf` (
   KEY `keyid` (`keyword_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+DROP TABLE IF EXISTS `search_pending`;
 CREATE TABLE IF NOT EXISTS `search_pending` (
   `site_id` int(11) DEFAULT NULL,
   `temp_id` varchar(32) DEFAULT NULL,
@@ -198,6 +222,7 @@ CREATE TABLE IF NOT EXISTS `search_pending` (
   `num` int(11) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+DROP TABLE IF EXISTS `search_query_log`;
 CREATE TABLE IF NOT EXISTS `search_query_log` (
   `query` varchar(255) DEFAULT NULL,
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -206,6 +231,7 @@ CREATE TABLE IF NOT EXISTS `search_query_log` (
   KEY `query_key` (`query`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+DROP TABLE IF EXISTS `search_sites`;
 CREATE TABLE IF NOT EXISTS `search_sites` (
   `site_id` int(11) NOT NULL AUTO_INCREMENT,
   `url` varchar(255) DEFAULT NULL,
@@ -217,13 +243,18 @@ CREATE TABLE IF NOT EXISTS `search_sites` (
   `disallowed` text,
   `can_leave_domain` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`site_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
+INSERT INTO `search_sites` (`site_id`, `url`, `title`, `short_desc`, `indexdate`, `spider_depth`, `required`, `disallowed`, `can_leave_domain`) VALUES
+(1, 'https://zongl.info/', 'slabikar', 'zongleruv slabikar', '2015-02-26', -1, '', 'prihlaseni.php?next=\n*/obrazky.*html/\n*/horoskop.*html/', 0);
+
+DROP TABLE IF EXISTS `search_site_category`;
 CREATE TABLE IF NOT EXISTS `search_site_category` (
   `site_id` int(11) DEFAULT NULL,
   `category_id` int(11) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+DROP TABLE IF EXISTS `search_temp`;
 CREATE TABLE IF NOT EXISTS `search_temp` (
   `link` varchar(255) DEFAULT NULL,
   `level` int(11) DEFAULT NULL,
