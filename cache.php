02d024c130d97e05c8097bc1264aaeebca36dc0f
Petr <petr@kle.cz>
2015-02-26 19:42:34 +0100

    Oprava chachování

--- cache.php
+++ cache.php
@@ -1,13 +1,19 @@
 <?php
 date_default_timezone_set('Europe/Prague');
 function http_cache_headers($expires,$login_vary=false){
-	global $_SERVER,$_SESSION;
 	if(!isset($_SESSION['logged']) or $login_vary){
 		$cas = gmdate('U',time());
 			header('Cache-Control: public, maxage=' . $expires);
 			header('Last-Modified: ' . gmdate('D, d M Y H:i:s', $cas-$expires) . ' GMT');
-			header('Expires: ' . gmdate('D, d M Y H:i:s', $cas+$expires) . ' GMT');
-		if(isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) && (strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE'])+(2*$expires)) > $cas and $_SERVER['SERVER_NAME']!='zongl.info'){
+			if(!isset($_COOKIE['ZS'])){
+				header('Expires: ' . gmdate('D, d M Y H:i:s', $cas+$expires) . ' GMT');
+			}
+			if(
+				isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) and
+			 	(strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE'])+($expires)) > $cas 
+				and $_SERVER['SERVER_NAME']!='zongl.info'
+				and !isset($_COOKIE['ZS'])
+			){
 			 header('HTTP/1.0 304 Not Modified');
 			 exit;
 		}
