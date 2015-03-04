1ddb894de578267e3664d5e6b8ea550e15250f89
Petr <petr@kle.cz>
2015-03-04 22:07:05 +0100

    Titulky událostí z facebooku

--- rss/update.php
+++ rss/update.php
@@ -36,7 +36,7 @@ foreach($rss_zdroje as $id=>$kanal){
 		}
 
 		if(preg_match('/facebook/',$kanal['feed_url'])){
-			$item['link']=preg_replace('/^\//','http://facebook.com/',$item['link']);
+			$item['link']=preg_replace('/^\//','https://facebook.com/',$item['link']);
 
 			if(strlen(trim($item['title']))==0 and isset($item['atom_content'])){
 				$ac=trim(strip_tags($item['atom_content']));
@@ -44,6 +44,30 @@ foreach($rss_zdroje as $id=>$kanal){
 					$item['title']=$ac;
 				}
 			}
+
+			if(strlen(trim($item['title']))==0 and preg_match('/facebook\.com\/events\//',$item['link'])){
+				$item['link'] = preg_replace('/^http:\/\/www\.facebook\.com/', 'https://www.facebook.com', $item['link']);
+				$dom = new DOMDocument();
+				$ch = curl_init();
+				curl_setopt($ch, CURLOPT_URL, $item['link']);
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+				curl_setopt($ch, CURLOPT_AUTOREFERER, 1); 
+				curl_setopt($ch, CURLOPT_REFERER, BASEURL);
+				curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 20);
+				curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.65 Safari/537.36');
+				curl_setopt($ch,CURLOPT_ENCODING, '');
+				$foo=curl_exec($ch);
+				$dom->loadHTML($foo);
+				curl_close($ch);
+
+				$xpath = new DOMXPath($dom);
+				$titleNode = $xpath->query('//title');
+				$title = $titleNode->item(0)->nodeValue;
+				if(strlen(trim($title)) > 0){
+					$item['title'] = preg_replace('/\s\|\sFacebook$/', '', $title);
+				}
+			}
+
 		}
 
 		if(isset($item['feedburner']['origlink'])){
