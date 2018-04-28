980f935eca56ad9d9cad756f38260c7acbeb9fed
Petr <petr@kle.cz>
2018-04-28 14:25:33 +0200

    Animace

--- static/mobile/stahni.sh
+++ static/mobile/stahni.sh
@@ -15,7 +15,7 @@ wget \
 	https://zongl.info/ \
 	https://zongl.info/img/e/external.png \
 	--reject 'pdf,juggling-tv.html,doplnky-prohlizece.html,odkazy.html,mobil.html,exkurze.html,vanoce-*.html,pf-*.html,changelog*.html,statistiky.html,odkazy.html,navody.html,changelog.html,rss.html,tip.html,jak-odkazovat.html' \
-	-X animace,video,obrazky,mapa,kalendar,ulita,lide,diskuse,obrazky-na-plochu,navody,scripts,horoskop,vyhledavani,tip,rss,download,olympiada,mdz,g,valentyn
+	-X animace/siteswap,animace/en,video,obrazky,mapa,kalendar,ulita,lide,diskuse,obrazky-na-plochu,navody,scripts,horoskop,vyhledavani,tip,rss,download,olympiada,mdz,g,valentyn
 
 rm -rf zongleruv-slabikar
 mv zongl.info zongleruv-slabikar
