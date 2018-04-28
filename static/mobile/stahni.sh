d18a03012650e4729406957decf7cefd9108fdab
Petr <petr@kle.cz>
2018-04-28 17:30:01 +0200

    Oprava stahování

--- static/mobile/stahni.sh
+++ static/mobile/stahni.sh
@@ -12,7 +12,7 @@ wget \
 	--ca-directory=/noexist \
 	https://zongl.info/ \
 	https://zongl.info/img/e/external.png \
-	--reject '*.pdf,juggling-tv.html,doplnky-prohlizece.html,odkazy.html,mobil.html,exkurze.html,vanoce-*.html,pf-*.html,changelog*.html,statistiky.html,odkazy.html,navody.html,changelog.html,rss.html,tip.html,jak-odkazovat.html' \
+	--reject '*.pdf,juggling-tv.html,doplnky-prohlizece.html,odkazy.html,mobil.html,exkurze.html,vanoce-*.html,pf-*.html,changelog*.html,statistiky.html,odkazy.html,navody.html,changelog.html,rss.html,tip.html,jak-odkazovat.html,toolbox.html' \
 	-X animace/siteswap,animace/en,video,obrazky,mapa,kalendar,ulita,lide,diskuse,obrazky-na-plochu,navody,scripts,horoskop,vyhledavani,tip,rss,download,olympiada,mdz,g,valentyn \
 	--convert-links \
 	--page-requisites
