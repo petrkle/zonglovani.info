7cbb398b640be23e53e70fa1aaa6b0ddac1eb584
Petr <petr@kle.cz>
2018-05-10 05:40:37 +0200

    Mazání starých mailů

--- cron/old-accounts.sh
+++ cron/old-accounts.sh
@@ -12,3 +12,14 @@ do
 	FL=`echo $MAIL | sed "s/^\(.\).*/\1/"`
 	rm -rf $DATADIR/lide/$UCET $DATADIR/lide.by.mail/$DOMAIN/$FL/$MAIL
 done
+
+for foo in `find $DATADIR/lide.by.mail -type f -name login -printf '%P\n'`
+do
+	LOGIN=`cat $DATADIR/lide.by.mail/$foo`
+	DOMAIN=`echo $foo | cut -d/ -f1`
+	ADR=`echo $foo | cut -d/ -f3`
+	MAIL="$ADR@$DOMAIN"
+	[ -f $DATADIR/lide/$LOGIN/$MAIL.mail ] || rm $DATADIR/lide.by.mail/$foo
+done
+
+find $DATADIR/lide.by.mail -type d -empty -delete
--- scripts/datasync.sh
+++ scripts/datasync.sh
@@ -3,8 +3,10 @@
 rsync -e ssh --recursive \
 	--rsync-path='nice rsync' \
 	--stats \
+	--human-readable \
 	--verbose \
 	--times \
+	--delete-after \
 	--update \
 	vps.kle.cz:/home/www/zonglovani.info/data/ \
 	/home/www/zonglovani.info/data/
