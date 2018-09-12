74879f460ad2368eef09a1cb33f154cb5c97977a
Petr <petr@kle.cz>
2018-09-12 20:14:15 +0200

    Tipy týdne

--- tip/pondelky.php
+++ tip/pondelky.php
@@ -2,7 +2,7 @@
 <?php
 date_default_timezone_set('Europe/Prague');
 for ($foo = 0; $foo < 800; ++$foo) {
-    $bar = strtotime('09/12/2016') + ($foo * 24 * 3600);
+    $bar = strtotime('10/1/2018') + ($foo * 24 * 3600);
     if (date('N', $bar) == 1) {
         echo date('Y-m-d', $bar)."\n";
     }
