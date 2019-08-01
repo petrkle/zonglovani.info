94429bd007be782203921d889278c0ed79d8f055
Petr <petr@kle.cz>
2019-08-01 21:34:11 +0200

    Doplnění odkazů v menu

--- static/mobile/stahni.sh
+++ static/mobile/stahni.sh
@@ -14,7 +14,7 @@ $WGET \
 	--ca-directory=/noexist \
 	https://zongl.info/ \
 	https://zongl.info/img/e/external.png \
-	--reject '*.pdf,juggling-tv.html,doplnky-prohlizece.html,odkazy.html,mobil.html,exkurze.html,vanoce-*.html,pf-*.html,changelog*.html,statistiky.html,odkazy.html,navody.html,changelog.html,rss.html,tip.html,jak-odkazovat.html,toolbox.html' \
+	--reject '*.pdf,odkazy.html,mobil.html,vanoce-*.html,pf-*.html,odkazy.html,navody.html,jak-odkazovat.html,toolbox.html' \
 	-X animace/siteswap,animace/en,obrazky,ulita,obrazky-na-plochu,navody,scripts,vyhledavani,download,olympiada,mdz,g,valentyn \
 	--convert-links \
 	--page-requisites
