2137fd9ef06608416d679e1064c7321e17e9867a
Petr <petr@kle.cz>
2019-11-17 10:35:23 +0100

    Obnova souboru

---
+++ scripts/stahni.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+URL=zongl.info
+SLOWDOWN=""
+
+[ $HOSTNAME = "vps" ] && URL=zonglovani.info SLOWDOWN="--random-wait --wait 1"
+
+wget \
+ $SLOWDOWN \
+ -e "robots=off" \
+ --directory-prefix=/tmp/zs.sitemap \
+ --user-agent="$URL/vyhledavani" \
+ --no-check-certificate \
+ --ca-directory=/noexist \
+ --no-host-directories \
+ --reject='*prihlaseni.php*' \
+ --accept=.html,.php,.xml \
+ --recursive \
+ --level=20 \
+  --quiet \
+ https://$URL/
