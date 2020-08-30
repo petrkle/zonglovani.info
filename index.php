af780408ee35a9bb77462649fea7bfcb79733185
Petr <petr@kle.cz>
2020-08-30 20:03:21 +0200

    Úprava popisu

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ require 'func.php';
 
 $smarty->assign('titulek', 'Žonglérův slabikář - žonglování s míčky, kruhy a kužely');
 $smarty->assign('nadpis', '');
-$smarty->assign('keywords', 'žonglování, míčky, kruhy, kužely, seznam žonglérů');
-$smarty->assign('description', 'Žonglování s míčky, kruhy a kužely. Seznamu žonglérů, kalendář žongléřských srazů a obrázky žonglování.');
+$smarty->assign('keywords', 'žonglování, míčky, kruhy, kužely');
+$smarty->assign('description', 'Žonglování s míčky, kruhy a kužely.');
 $smarty->assign('notitle', true);
 $smarty->assign('stylwidth', IMG_CSS_WIDTH);
 
